58cc181faaf1181773d59b7f9edac0ba6d308451
davidevans <davidevans>
2010-06-16 15:23:51 +0000

    Add some expanation, fix a typo.

--- NOTES/fossil-binary.sh
+++ NOTES/fossil-binary.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Fossil needs to be told about binary files so it doesn't try to merge them
-# I don't know that to do about Vienna Help idx in the Vienna Help directory
+# Run this script after you have set up a new Repo.
+
+# Fossil needs to be told about binary files so it doesn't try to merge them.
+# I don't know what to do about Vienna Help idx in the Vienna Help directory.
 
 BINARIES=*.tiff,*.jpeg,*.jpg,*.gif,*.nib,*.xib,*.tar,rz,sz,sqlite2,\
 sqlite3,*.icns,*.plist,*.a,*.o,*.lo,*.la,*.rtf,*.pbxproj,*.pch,\
