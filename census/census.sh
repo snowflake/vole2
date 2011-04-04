3c12deaf2e8cb39f85cdf96cb35c286598a5f96f
davidevans <davidevans>
2011-04-04 22:12:07 +0000

    census: general reorganization

--- census/census.sh
+++ census/census.sh
@@ -7,15 +7,22 @@
 # Author: devans
 # Created 2011-03-02
 
+scriptversion=1.7
+
+
 # use Apple version of the utilities. Avoid Macports,Fink or Darwinports
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
 # Note: I don't know how long bzip2 has been available on OS X
 # therefore gzip is used to compress the log.
-scriptversion=1.6
 # send results to TO
 TO='dave.evans55@googlemail.com'
 T=temporary-file
+clear
+echo
+echo
+echo "Vienna census and log reporter version ${scriptversion}"  
+echo 
 which -s uuidgen
 if [ $? -ne 0 ]
 then
@@ -31,7 +38,7 @@ then
 manifest=`cat manifest.uuid`
 fi
 reportid=`uuidgen`
-datenow=`TZ=UTC date +%Y-%m-%dT%H:%M:%S-%Z`
+datenow=`TZ=UTC date +%Y-%m-%dT%H:%M:%SZ`
 G="grep -i Vienna"
 H=${reportid}.${datenow}
 
@@ -93,7 +100,6 @@ echo Thanks for your participation. Your data have not been sent.
 exit 0
 fi
 
-logger "Vienna census script version ${scriptversion} started for report ${H}"
 
 
 echo Cix user nickname: $nick > $T
@@ -104,14 +110,22 @@ echo Script version: $scriptversion  >> $T
 echo Unique report ID: ${reportid} >> $T
 echo Cookie: ${Cookie} >> $T
 echo Fossil manifest SHA1: ${manifest} >> $T
-echo $H
-
+echo >> $T
+echo '=== Begin uname ===' >> $T
 uname -a >> $T
+echo '=== End uname ===' >> $T
+echo >> $T
+echo === Begin Mac OS X version === >> $T
+sw_vers >> $T
+echo === End Mac OS X version === >> $T
+
 echo >> $T
 echo === Begin xcodebuild === >> $T
 [ -x /usr/bin/xcodebuild ] && xcodebuild -version >> $T
 [ -x /usr/bin/xcode-select ]  && xcode-select -print-path >> $T
 echo === End xcodebuild === >> $T
+
+echo >> $T
 
 echo === Begin developer tools === >> $T
 if [ -x /usr/sbin/system_profiler ]
@@ -125,19 +139,28 @@ echo === End developer tools === >> $T
 
 
 echo >> $T
+echo '=== Begin system log files count ===' >> $T
+GFC=`ls  ${L}.[0-9]*.gz 2>/dev/null | wc -l`
+echo System log gzip files: ${GFC} | tee -a $T
+BFC=`ls ${L}.[0-9]*.bz2 2>/dev/null | wc -l`
+echo System log bzip files: ${BFC} | tee -a $T
+echo === End system log files count === >> $T
+
+echo >> $T
 echo '=== Begin system log for Vienna ===' >> $T
-FC=`ls  ${L}.[0-9]*.gz 2>/dev/null | wc -l`
-echo gzip files ${FC}
-[ $FC -gt 0 ] && \
+[ $GFC -gt 0 ] && \
 gzcat ${L}.[0-9]*.gz |  $G  >> $T
 
-FC=`ls ${L}.[0-9]*.bz2 2>/dev/null | wc -l`
-echo bzip files ${FC}
-[ $FC -gt 0 ] && which bzcat && \
+[ $BFC -gt 0 ] && which -s bzcat && \
 bzcat ${L}.[0-9]*.bz2  |  $G >> $T
 
 [ -f ${L} ] && cat ${L} | $G >> $T
 echo '=== End system log for Vienna ===' >> $T
+
+echo >> $T
+echo '=*= End of report =*=' >> $T
+
+
 
 # because the administrator does not have permission to access other
 # users files, the crash reporter filelist has been deleted
@@ -153,5 +176,6 @@ echo alter the message header or body in any way.
 echo
 echo There will now be a brief pause while you read this message  ...
 sleep 10
+logger "Vienna census script version ${scriptversion} sending report ${H}"
 echo Starting email app.
 open "mailto:${TO}?subject=Vienna%20census%20${H}"
