7775db3171cf9a4e8cecdf8b35cb99580a03f796
root <root>
2011-04-26 15:42:35 +0000

    Vienna: add the string sanitiser

---
+++ Vienna/sanitise_string.c
@@ -0,0 +1,18 @@
+#import "sanitise_string.h"
+#import "sanitise_string_private.h"
+
+char           *
+sanitise_string(char *p)
+{
+
+	/*
+	 * remove chars not defined by Windows CP1252 from C-strings before
+	 * feeding them to NSString conversion routines
+	 */
+	char           *cp = p;
+	if (p == NULL)
+		return p;
+	while ((c = *cp) != '\0')
+		*cp++ = cp1252_table[c];
+	return p;
+}
