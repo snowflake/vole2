3d4a5cbe73da962f2b0ee7d7609741eaf12e782f
davidevans <davidevans>
2011-11-24 23:07:24 +0000

    add checking for multiple instances of Vienna - not yet finished.

---
+++ Vienna/CheckVienna.h
@@ -0,0 +1,16 @@
+//
+//  CheckVienna.h
+//  Vienna
+//
+//  Created by David Evans on 24/11/2011.
+//  Copyright 2011 David Evans. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CheckVienna : NSObject {
+	
+}
+-(void)awakeFromNib;
+@end
