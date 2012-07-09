68ff5924e1e87a73642112f51228df58c99ff8f2
davidevans <davidevans>
2012-07-09 23:07:32 +0000

    fossil-binary.sh: add *.tar.gz and *.tgz

--- scripts/fossil-binary.sh
+++ scripts/fossil-binary.sh
@@ -5,7 +5,8 @@
 # Fossil needs to be told about binary files so it doesn't try to merge them.
 # I don't know what to do about Vienna Help idx in the Vienna Help directory.
 
-BINARIES=*.tiff,*.jpeg,*.jpg,*.gif,*.nib,*.xib,*.tar,rz,sz,sqlite2,\
+BINARIES=*.tiff,*.jpeg,*.jpg,*.gif,*.nib,*.xib,\
+*.tar,*.tar.gz,*.tgz,rz,sz,sqlite2,\
 sqlite3,*.icns,*.plist,*.a,*.o,*.lo,*.la,*.rtf,*.pbxproj,*.pch,\
 *.xml,*.pbxuser,*.mode1,*.perspective,*.perspectivev3,*.scriptTerminology,\
 *.xcodeproj,*idx
