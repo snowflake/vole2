c3ed229da0ec679fbf6f52492a9d8978bbe161b3
davidevans <davidevans>
2012-08-07 15:25:29 +0000

    export-to-git.mk: a makefile to export from fossil to a git repo

---
+++ mk/export-to-git.mk
@@ -0,0 +1,11 @@
+# export the repository to git
+
+dir=../../vole-git
+marks=/tmp/marks
+
+all:
+	mkdir -p ${dir}
+	fossil export --git  > /tmp/vex
+	
+	cat /tmp/vex \
+	| ( cd ${dir} && git init && git fast-import --quiet  )
