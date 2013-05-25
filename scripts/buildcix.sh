d7ff0fa8a590033b61ed3a437ed85cea93b63885
davidevans <davidevans>
2013-05-25 15:23:14 +0000

    Makefile and buildcix.sh: New way of specifying Deployment target, build archs and 64 bit compatibility. The SDK to use can be specified for each target.

--- scripts/buildcix.sh
+++ scripts/buildcix.sh
@@ -7,6 +7,43 @@
 # $3 = name of file to write
 # $4 = compatibility note
 # $5 = status
+# $6 = deployment target (10.4 10.5 etc)
+# $7 = build archs (ppc i386 x86_64)
+# $8 = SDK (macosx10.4 etc )
+
+deploy="${6}"
+archs="${7}"
+sdk="${8}"
+
+function list_minimum_osx(){
+printf "Minumum OS X required: "
+case ${deploy} in 
+	10.4)   printf "10.4 Tiger and later";;
+	10.5)   printf "10.5 Leopard and later";;
+	10.6)   printf "10.6 Snow Leopard and later";;
+	10.6.8) printf '10.6.8 Snow Leopard (last version) and later';;
+	10.7)   printf "10.7 Lion and later";;
+	10.8)   printf "10.8 Mountain Lion and later";;
+        *         )  printf "Unknown";;
+esac
+}
+
+function list_archs(){
+printf "Runs On:"
+native=No
+for i in ${archs}
+do
+case $i in
+	ppc)    ppc=" PowerPC" ;;
+	i386)   i32=" Intel-32-bit" ; i64=" Intel-64-bit" ;;
+	x86_64) i64=" Intel-64-bit" ; native=Yes ;;
+esac
+done
+printf "%s%s%s\n" "${ppc}" "${i32}" "${i64}"
+printf "Native Intel-64-bit: %s" $native
+}
+##### end of function #####
+
 cat > $3  << XEOF
 New file $2
 
@@ -21,9 +58,11 @@ $(openssl sha1 $2)
 Description: Vole off-line reader for Mac OS X only
              Vole was formerly known as Vienna or Vinkix
 Version: $($1 -m)
-$4
+$(list_minimum_osx)
+$(list_archs)
 $5
 Packaging: A zip file containing a disk image and an OpenPGP signature
+SDK: ${sdk}
 Checkout: $( $1 -c)
 Build: $($1 -b)
 $(dwarfdump --uuid $1 | awk '{ print $1, $2, $3 }')
@@ -32,19 +71,15 @@ $(dwarfdump --uuid $1 | awk '{ print $1, $2, $3 }')
 Contributor: devans
 Date: $(date '+%A %e %B %Y') 
 
-Release and Installation notes for all versions are available from
-http://chiselapp.com/user/vinkix/repository/olrcix/wiki/RINOTES
-
-[OpenPGP verification]
-The disk image contained in the zip file is signed with an OpenPGP key.
-The key can be obtained from the SKS keyservers.
-Key ID: 0xC87001EA
-Key fingerprint = 261B D1C6 DCEF 9281 7495  A9B5 7C5F 87B6 C870 01EA
-uid David Evans <dave.evans55@googlemail.com>
-
 [Warning]
-Windows and Linux users please note that this file is not much
-use to you.
+***********************************************************************
+To avoid disappointment and a wasted download, please check the 
+Minimum OS X Required and Runs On fields above to make sure
+that Vole will be compatible with your system.
+ 
+Previous versions of Vole/Vinkix/Vienna were compatible with everything
+from Tiger upwards. This is no longer the case.
+***********************************************************************
 
 [Notes]
 $(/opt/local/bin/lynx -dump ../NOTES/RELEASE.html)
