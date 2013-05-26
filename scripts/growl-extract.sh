7ff13bc4b20d09e47d9580f59fa8c22224f0be9a
davidevans <davidevans>
2013-05-26 13:34:43 +0000

    Growl Extract. The the tar to extract based on SDK name. Previous versions used Deployment Target.

--- scripts/growl-extract.sh
+++ scripts/growl-extract.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 # script to extract appropiate version of growl, depending on 
-# OSX deployment version.
+# OSX SDK.
 
 rm -rf Growl.framework
 
-if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]
+if [ -z "${SDK_NAME}" ]
 then
-	echo Error: need to set MACOSX_DEPLOYMENT_TARGET 
+	echo Error: need to set SDK_NAME 
 	exit 1
 fi
- 
-case $MACOSX_DEPLOYMENT_TARGET in
-	( 10.4      ) tar xvf growl0.7.4.tar            ;;
-	( 10.5|10.6 ) tar xvzf ../growl/growl-1.2.3.tgz ;;
+
+rm -rf Growl.framework 
+
+case ${SDK_NAME} in
+	( macosx10.4      ) tar xvf growl0.7.4.tar            ;;
+	( macosx10.5|macosx10.6|macosx10.7|macosx10.8 ) 
+				tar xvzf ../growl/growl-1.2.3.tgz ;;
+	 * ) echo "Error: Growl Extract: unknown SDK ${SDK_NAME}"; exit 1 ;;
 	esac
 if [ $? -ne 0 ]
 then
