ebd4c97beaa523f82057087e3db984492fb0199b
davidevans <davidevans>
2013-10-31 11:23:17 +0000

    export-to-git: Make sure we are in the master source directory. (In case we are running this from another source location.)

--- mk/export-to-git.mk
+++ mk/export-to-git.mk
@@ -3,12 +3,16 @@
 dir=../../vole-git
 marks=/tmp/marks
 
-all:
+all: directory-check
 	mkdir -p ${dir}
 	fossil export --git  > /tmp/vex
 	cat /tmp/vex \
 		| ( cd ${dir} && git init && git fast-import --quiet  )
 	-(cd ${dir} && git remote add origin git@github.com:snowflake/vole.git)
 
-push:
+push: directory-check
 	cd ${dir} && git push --mirror
+
+# check that we are in the right folder - the master source for fossil
+directory-check:
+	[ `pwd` = /Users/davidevans/Vienna-source/xxx/mk ]
