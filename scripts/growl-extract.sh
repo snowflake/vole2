86ebdb59aa0eddc0092f08f9b50ecaa80319a667
davidevans <davidevans>
2013-11-03 15:00:36 +0000

    Extract Growl framework from zip files for 2.0.1 and 1.3.1

--- scripts/growl-extract.sh
+++ scripts/growl-extract.sh
@@ -3,6 +3,21 @@
 # script to extract appropiate version of growl, depending on 
 # OSX SDK.
 
+growltemp=growl-temporary-extract-directory
+
+function zipextract(){
+	# Note: There are two frameworks within these zips - current and legacy
+	#       This function is only extracting the current framework
+	echo extracting Growl framework version ${1}
+	mkdir -p /tmp/${growltemp}
+	rm -rf /tmp/${growltemp}/*
+	unzip ../growl/Growl-${1}-SDK.zip -d /tmp/${growltemp}
+	mv /tmp/${growltemp}/Growl-${1}-SDK/Framework/Growl.framework .
+	rm -rf /tmp/${growltemp}/*
+	rmdir  /tmp/${growltemp}
+}
+### end of function zipextract ###
+
 rm -rf Growl.framework
 
 if [ -z "${SDK_NAME}" ]
@@ -11,16 +26,15 @@ then
 	exit 1
 fi
 
-rm -rf Growl.framework 
-
 case ${SDK_NAME} in
-	( macosx10.4      ) tar xvf growl0.7.4.tar            ;;
-	( macosx10.5|macosx10.6|macosx10.7|macosx10.8|macosx10.9 ) 
-				tar xvzf ../growl/growl-1.2.3.tgz ;;
-	 * ) echo "Error: Growl Extract: unknown SDK ${SDK_NAME}"; exit 1 ;;
+	( macosx10.4      )       tar xvf growl0.7.4.tar ;;
+	( macosx10.5|macosx10.6 ) tar xvzf ../growl/growl-1.2.3.tgz ;;
+	( macosx10.7|macosx10.8|macosx10.9 ) zipextract 2.0.1 ;;
+	(  * ) echo "Error: Growl Extract: unknown SDK ${SDK_NAME}"; exit 1 ;;
 	esac
 if [ $? -ne 0 ]
 then
 	echo Error extracting in growl-extract.sh.
 	exit 1
 fi
+exit 0
