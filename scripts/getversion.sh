d7954df85edd6ab193f561af831218c79f853302
davidevans <davidevans>
2013-11-25 10:38:17 +0000

    getversion.sh: Use PlistBuddy instead of agvtool to get the version for release builds. agvtool does not work if there are mulitple projects in the Vienna directory.

--- scripts/getversion.sh
+++ scripts/getversion.sh
@@ -3,5 +3,9 @@ PATH=${DEVELOPER_DIR}/usr/bin:${PATH}
 
 # get the version from Info.plist
 
-agvtool mvers |  tr '"' '|' \
-	| awk  'BEGIN { FS="|" } /Found CFBundle/ { printf "%s", $2 }'
+# # agvtool does not work with multiple projects in the same directory
+#agvtool mvers |  tr '"' '|' \
+#	| awk  'BEGIN { FS="|" } /Found CFBundle/ { printf "%s", $2 }'
+
+# Use PlistBuddy instead
+/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" Info.plist
