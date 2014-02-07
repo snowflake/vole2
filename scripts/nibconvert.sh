d25e7920e6688bff53a8e0a0becb82661f96ccfd
davidevans <davidevans>
2014-02-07 21:28:18 +0000

    Add a '------' line for nibconvert script

--- scripts/nibconvert.sh
+++ scripts/nibconvert.sh
@@ -6,8 +6,9 @@
 # You will need to add the xib files to the project and delete the nib files.
 
 
-echo Processing $1
-/Xcode3.2.6/usr/bin/ibtool $1 --upgrade --write $(basename $1 .nib).xib 
+echo '----------------------' Processing $1
+/Xcode3.2.6/usr/bin/ibtool $1 --upgrade --errors --warnings \
+	--write $(basename $1 .nib).xib 
 echo return code for $1 was $?
 # reset the 'file extension is hidden' attribute
 SetFile -a e $(basename $1 .nib).xib
