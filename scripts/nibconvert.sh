326aee9c57011c5c79e7110343e2bc4ee9a81c85
davidevans <davidevans>
2014-02-08 11:03:37 +0000

    nibconvert - add notices and human-readable text to diagnostic output

--- scripts/nibconvert.sh
+++ scripts/nibconvert.sh
@@ -8,6 +8,7 @@
 
 echo '----------------------' Processing $1
 /Xcode3.2.6/usr/bin/ibtool $1 --upgrade --errors --warnings \
+	--notices --output-format human-readable-text \
 	--write $(basename $1 .nib).xib 
 echo return code for $1 was $?
 # reset the 'file extension is hidden' attribute
