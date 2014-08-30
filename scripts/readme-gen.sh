4c7cb688b38b03aef84ce8030b5da2da266a94a3
davidevans <davidevans>
2014-08-30 14:38:08 +0000

    Simplify the script that generates README.html. Remove references to GPG.

--- scripts/readme-gen.sh
+++ scripts/readme-gen.sh
@@ -16,17 +16,6 @@ XEOF
 printf "%-20s %s\n" "README.html" "What you are reading now."
 printf "%-20s %s\n" "$1.dmg.sig" "A detached OpenPGP signature to check the disk image."
 printf "%-20s %s\n" "$1.dmg"     "A disk image containing Vole."
-cat << X2EOF
-
-You can use your OpenPGP utility (for example: GnuPG) to check the disk image
-against its cryptographic signature contained in the signature file. This
-step is completely optional.
-
-The key that was used to sign the disk image can be found by searching the key
-servers for the key of dave.evans55@googlemail.com.	
-
-GnuPG can be found at <a href="http://www.gnupg.org/">http://www.gnupg.org/</a>
-
-</pre>
-</body>
-X2EOF
+printf '\n\n'
+printf 'Click on the disk image file "%s" to open it.\n' "$1.dmg"
+printf '</pre>\n</body>\n'
