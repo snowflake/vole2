2498f88177bb951c864b3114259dac41366c5833
davidevans <davidevans>
2015-11-27 14:09:50 +0000

    sha-hashes.sh: added a script to compute SHA-X hashes on a file and sign the hashes with GPG.

---
+++ scripts/sha-hashes.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# script to compute SHA-x hashes for the disk image.
+# Environment variable VOLE_PGP_USER1 and VOLE_PGP_USER2 should
+#  be set to the pgp user(s) hex key identities
+
+if [ -n "${VOLE_PGP_USER1}" ]
+then
+USER1="-u ${VOLE_PGP_USER1}"
+fi
+
+if [ -n "${VOLE_PGP_USER2}" ]
+then
+USER2="-u ${VOLE_PGP_USER2}"
+fi
+
+
+rm -f temp-hash
+for i in 1 224 256 384 512
+do
+/usr/bin/shasum --algorithm $i --binary $1 >> temp-hash
+done
+
+gpg --clearsign  ${USER1} ${USER2}  --armor --digest-algo SHA512 temp-hash
+
