277ca76699587e88c18ef1b52b610ea48d4ee112
davidevans <davidevans>
2016-05-11 07:29:50 +0000

    Update the script that generates README.html for the zip file.

--- scripts/readme-gen.sh
+++ scripts/readme-gen.sh
@@ -10,12 +10,13 @@ cat << XEOF
 This is the zip archive for the Vole project, an Off-Line reader
 for Cix.
 
-This zip archive contains 3 files:
+This zip archive contains 4 files:
 
 XEOF
-printf "%-20s %s\n" "README.html" "What you are reading now."
-printf "%-20s %s\n" "$1.dmg.sig" "A detached OpenPGP signature to check the disk image."
-printf "%-20s %s\n" "$1.dmg"     "A disk image containing Vole."
+printf "%-22s %s\n" "disk-image-hashes.asc" "SHA hashes of $1.dmg"
+printf "%-22s %s\n" "README.html" "What you are reading now."
+printf "%-22s %s\n" "$1.dmg.sig" "A detached OpenPGP signature to check the disk image."
+printf "%-22s %s\n" "$1.dmg"     "A disk image containing Vole."
 printf '\n\n'
 printf 'Click on the disk image file "%s" to open it.\n' "$1.dmg"
 printf '</pre>\n</body>\n'
