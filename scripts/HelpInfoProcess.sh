c1146711df190cce459c5ad6d5a9515bdc42528c
davidevans <davidevans>
2018-02-06 16:10:24 +0000

    Help Book: Add some corrections to make books for Vole1 and Vole2 have separate identities (CFBundleHelpBookName). This stops Vole1 using helpbook for Vole2 and vice versa.

--- scripts/HelpInfoProcess.sh
+++ scripts/HelpInfoProcess.sh
@@ -3,9 +3,10 @@
 # Script to update the Info plist in the Help Book to the latest
 # version and identifier.
 # Argument 1  is the location of the built App.
+# Argument 2  is the Help Book Name.
 
 APPDIR=${1}
-
+HelpBookName="${2}"
 AppName=Vole.app
 HelpName=Vole.help
 PLB=/usr/libexec/plistbuddy
@@ -42,3 +43,11 @@ ${PLB} -x -c "Set :CFBundleShortVersionString ${Version}" ${HelpInfo}
 # Set the Help Identifier
 ${PLB} -x -c "Set :CFBundleIdentifier ${HelpIdentifier}" ${HelpInfo}
 
+# Set the Help Book Name
+if [ "${HelpBookName}" = "" ]
+then
+    echo "Need help book name to be set"
+    exit 1
+fi
+${PLB} -x -c "Set :CFBundleName ${HelpBookName}" ${HelpInfo}
+${PLB} -x -c "Set :HPDBookKBProduct ${HelpBookName}"   ${HelpInfo}
