cf589fcdef7399dc3196050f74ef968a83db2de4
davidevans <davidevans>
2019-01-25 15:36:05 +0000

    Use rxp instead of opensp to validate xhtml1 files in Vole.help

--- mk/validate-html.mk
+++ mk/validate-html.mk
@@ -8,13 +8,14 @@ validate:
 	export SGML_CATALOG_FILES=/opt/local/share/sgml/html/catalog
 	export SP_BCTF=utf-8
 	set -e
-	cd "English.lproj/Vienna Help"
+	cd "Vole.help/Contents/Resources/English.lproj"
 	pwd
-	for i in *.html
+	for i in *.html pgs/*.html
 	do
 	echo $$i
-	/opt/local/bin/onsgmls -s $$i
+#	/opt/local/bin/onsgmls -s $$i
+	/opt/local/bin/rxp -sV $$i
 	tidy -q -utf8 1>/dev/null $$i
 	done
 	echo 'If you see this then there are no validation errors.'
-	
+
